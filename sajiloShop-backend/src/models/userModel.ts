7f166b801264332fa1164cd8699db3186c9eed92
Uttam Pulami <pulamimagaruttam@gmail.com>
2026-03-25 10:07:43 +0545

    usermode

--- sajiloShop-backend/src/models/userModel.ts
+++ sajiloShop-backend/src/models/userModel.ts
@@ -1,1 +1,40 @@
-import sequelize from "../config/db";   
+import sequelize from "../config/db";  
+import { DataTypes , Model ,InferAttributes , InferCreationAttributes } from 'sequelize';
+
+
+
+class User extends Model<InferAttributes<User>, InferCreationAttributes<User>> {
+    declare userName : string;
+    declare email: string;
+    declare password: string;
+}
+
+User.init(
+  {
+    // Model attributes are defined here
+    userName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      primaryKey: true
+        },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+         isEmail: true,    
+      }
+    },
+    
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+   
+    sequelize, // We need to pass the connection instance
+    modelName: 'User', // We need to choose the model name
+  },
+);
