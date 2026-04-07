bb58259d8d78a6779151a97a674d7cf5f0c103cc
Uttam Pulami <pulamimagaruttam@gmail.com>
2026-04-07 11:07:12 +0545

    folder structure

--- frontend/src/app/features/home/pages/home/home.ts
+++ frontend/src/app/features/home/pages/home/home.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { HeroSection } from "../../components/hero-section/hero-section";
 import { CategoriesSection } from "../../components/categories-section/categories-section";
 import { TrendinginNepalSection } from "../../components/trending-in-nepal-section/trending-in-nepal-section";
+import { ProductCard } from "../../../../shared/components/product-card/product-card";
 
 @Component({
   selector: 'app-home',
-  imports: [HeroSection, CategoriesSection, TrendinginNepalSection],
+  imports: [HeroSection, CategoriesSection, TrendinginNepalSection, ProductCard],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
